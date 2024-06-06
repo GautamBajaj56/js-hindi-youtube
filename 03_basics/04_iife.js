c59d01bf29548abd9a8c7369d2443192a8cc3f7a
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-06-06 15:24:08 +0530

    my notes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,35 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+/* ++++++++++++++++++  lecture 24++++++++++++++++++++ */
+
+/* concept about IIFE and why do we need them:
+1. these are those functions jo immediately execute ho jaaye
+2. and to reduce pollution due to global scope(i.e. variables methods declared in 
+global scope sometimes cause problem so to prevent its pollutio we use IIFE) */
+
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();    /* an IIFE where first () denotes function definition and second () denotes
+            function call */
+
+
+
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh') // unnamed or simple IIFE with a parameter 
+
+( () => {
+    console.log(`DB CONNECTED TWO `);
+} )()      // simple IIFE with no parameter
+
+
+/* note if humare function mei only one IIFE then no need of semicolon in line 14
+but if multiple IIFE are there in a program file and you want that every IIFE should 
+work properly then semicolon is must. */
+
+
+/* ++++++++++++++++++++++++++++lecture finish++++++++++++++++++++++++++++++ */
 
