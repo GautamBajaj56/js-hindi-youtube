89d8ffafeca68554db12995de9ffffec653556a1
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-06-24 16:00:09 +0530

    my notes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,41 @@
-// console.log(2 > 1);
+/* ++++++++++++++++++++++lecture 8++++++++++++++++++++ */
+
+
+// console.log(2 > 1);         /* basic no neeed to cover */
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+ console.log("2" > 1);// true as conversion took place
+ console.log("02" > 1);//true as conversion took place
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);// false
+console.log(null == 0);//false
+console.log(null >= 0);//true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+/* the reason is that an equality check == and comparisons
+>,<,>= ,<= work differently. comparisons convert null to
+a number, treating it as 0. that's why null>=0 is true
+and null>0 is false */
+
+console.log(undefined == 0); // false in every case
+console.log(undefined > 0);// false
+console.log(undefined < 0);// false
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); //false as ek string doosra number
+
+/* ===(triple equal to)basically compares value as well as datatype (called strict 
+check thats why)
+== mei datatype conversion ho jaata tha while comparing but in === no conversion
+takes place  */
+
+/* an imp note 
+avoid any comparisons with inconsistensies like comparison with null or undefined
+as they may lead to unexpected results  */
+
+
+/* ++++++++++++++++++++++++++lecture 8 finished++++++++++++++++++++++++++++++ */
