919668b88b28b842e39af94ab01fc7dd5ab4c691
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-06-26 16:24:26 +0530

    my notes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,11 +1,19 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+/* +++++++++++++++++++++++++lecture 17+++++++++++++++++++++++++++++++ */
 
-tinderUser.id = "123abc"
+/* this lecture is about creating object via constructor method
+ */
+
+// const tinderUser = new Object()
+// console.log(tinderUser);     output:{} (singleton object created)
+
+const tinderUser = {} // another way
+// console.log(tinderUser);    same output:{} (but non singleton object created)
+
+tinderUser.id = "123abc"          //adding key value pairs
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+ console.log(tinderUser); //{ id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -16,18 +24,35 @@ const regularUser = {
         }
     }
 }
+/* above example shows object k andar object bhi possible and jitni marzi nesting kar 
+sakte hai usme bhi
+nested  objects ko access karne k liye dot(.) operator use karte jaao object k andar 
+ghuste jaao like below  */
 
-// console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.firstname); //hitesh
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+let obj3 = { obj1, obj2 } //trying to concatenate objects
+console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+/* same array wali problem not desired result */
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+obj3 = Object.assign({}, obj1, obj2, obj4)
+console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+/* Copy the values of all of the enumerable own properties from one or more source 
+objects to a target object. Returns the target object */
+
+/* here {}is the target and obj1,obj2,obj4 are the source. target mei combine hoke
+final result store hoga 
+bina {} k bhi kaam chal jaata fark itna hota ki obj1 target banjaata and usme 
+modification ho jaati output same hi aata */
+
+
+ obj3 = {...obj1, ...obj2} // spread operator (best 90% cases mei yahi use hoga)
+ console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 
 const users = [
@@ -45,14 +70,29 @@ const users = [
     },
 ]
 
-users[1].email
-// console.log(tinderUser);
+/* here array mei multiple objects */
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+users[1].email // accessing in above situation
+// console.log(tinderUser);  { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+ console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+ /* it has returned keys packed in an array so can apply methods of array over it */
+
+ console.log(Object.values(tinderUser)); // [ '123abc', 'Sammy', false ]
+ console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+
+ /* sometimes we are trying to access some property but if it does not exist then 
+ program crash hone k chances hote hai so we  can check the existence of any property 
+ using below method */
+ console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
+
+
+ /* +++++++++++++++++++++++++++lecture 17 finished++++++++++++++++++++++++++++ */
+
+ /* +++++++++++++++++++++++++lecture 18+++++++++++++++++++++++++++++++++++++++ */
+
+ /* Object de-structure and JSON API intro */
 
 
 const course = {
@@ -61,12 +101,18 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
+course.courseInstructor  /* totally fine but problem will arise when multiple times
+course instructor print karvaana ho then baar baar course.courseInstructor itna lamba
+likhna will reduce code readabilty */
 
-const {courseInstructor: instructor} = course
+const {courseInstructor} = course  // object destructuring
+console.log(courseInstructor); // hitesh
 
-// console.log(courseInstructor);
-console.log(instructor);
+/* itna lamba name likhne se better ise nickname de sakte for short like below */
+const {courseInstructor: instructor} = course  // object destructuring
+console.log(instructor); // hitesh
+
+/* +++++++++++++some API discussion+++++++++++++++++++++++++++ */
 
 // {
 //     "name": "hitesh",
@@ -79,4 +125,6 @@ console.log(instructor);
     {},
     {}
 ]
+
+/* ++++++++++++++++++++++++++++++++lecture 18 finished+++++++++++++++++++++++++ */
 
