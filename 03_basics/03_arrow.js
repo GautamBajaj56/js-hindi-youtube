d8c1635fd01094d1f5d6ad638544e1985c00f4ab
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-06-27 10:59:09 +0530

    my notes

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,3 +1,15 @@
+/* +++++++++++++++++++++++++++Lecture 23 ++++++++++++++++++ */
+
+/* in this lec will be studying about "this" keyword and 
+how to declare arrow functions */
+
+/* in line we just want to add the username of object given to us so 
+have to use this keyword */
+
+/* learn about this keyword more in ms word file */
+
+/* this keyword in short current context ko refer karta hai  */
+
 const user = {
     username: "hitesh",
     price: 999,
@@ -9,46 +21,79 @@ const user = {
 
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+user.welcomeMessage() // hitesh, welcome to website
+/* {
+  username: 'hitesh',
+  price: 999,
+  welcomeMessage: [Function: welcomeMessage]
+} */
+user.username = "sam"
+user.welcomeMessage()// sam, welcome to website
+/* {
+  username: 'sam',
+  price: 999,
+  welcomeMessage: [Function: welcomeMessage]
+} */
 
-// console.log(this);
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+/* above example shows ki username hard coded nahi tha if changes kiye then reflect 
+bhi hue due to this keyword as this current instance/context ko point karta hai   */
 
-// chai()
+console.log(this); //{} (an empty object)
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+/* but if webpage cosole pe jaake karte then window object return karta */
+
+function chai(){
+    let username = "hitesh"
+    console.log(this.username);
+}
+
+chai()
+
+/* normal function declaration as we know below */
+
+const chai = function () {
+    let username = "hitesh"
+    console.log(this.username); // undefined as this does not work inside functions
+                                // works only in objects 
+}
+
+/* arrow function declaration  below */
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this);  //{}      this will not work inside functions 
+                       //works only in objects
 }
 
 
-// chai()
+chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+/* arrow function banane k liye simply remove function keyword and add arrow */
 
-// const addTwo = (num1, num2) =>  num1 + num2
+//() => {} a simpple arrow function jo abhi kisi variable mei stored nahi hai
+// const add = ()=>{} here store karva diya 
+// {} iske andar function definition aayegi 
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+const addTwo = (num1, num2) => { /* when braces used return statement
+                                           is must */
+    return num1 + num2
+}
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+ addTwo = (num1, num2) =>  num1 + num2   // no return statement required
+
+ addTwo = (num1, num2) => ( num1 + num2 )// no return statement required
+                                        // when () are used 
+
+
+ addTwo = (num1, num2) => ({username: "hitesh"}) //no return statement required
 
 
 console.log(addTwo(3, 4))
 
 
-// const myArray = [2, 5, 3, 7, 8]
+const myArray = [2, 5, 3, 7, 8]
 
-// myArray.forEach()
+myArray.forEach()
+
+/* ++++++++++++++++++++++++lecture 23 finished++++++++ */
