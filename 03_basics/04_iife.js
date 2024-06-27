7f25de41d3420c67d9d7d56f7a774695e9424205
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-06-27 11:10:31 +0530

    my notes

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -5,14 +5,15 @@
 /* concept about IIFE and why do we need them:
 1. these are those functions jo immediately execute ho jaaye
 2. and to reduce pollution due to global scope(i.e. variables methods declared in 
-global scope sometimes cause problem so to prevent its pollutio we use IIFE) */
+global scope sometimes cause problem so to prevent its pollution we use IIFE) */
 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();    /* an IIFE where first () denotes function definition and second () denotes
-            function call */
+           function call */
+           /* ()() first wala for function definition second wala for function call */
 
 
 
@@ -28,7 +29,7 @@ global scope sometimes cause problem so to prevent its pollutio we use IIFE) */
 
 /* note if humare function mei only one IIFE then no need of semicolon in line 14
 but if multiple IIFE are there in a program file and you want that every IIFE should 
-work properly then semicolon is must. */
+work properly then semicolon is must as semicolon marks the termination of one IIFE */
 
 
 /* ++++++++++++++++++++++++++++lecture finish++++++++++++++++++++++++++++++ */
