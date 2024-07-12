a99a1718ee58ecf838f4b108f83042764daa82b4
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-07-12 18:29:54 +0530

    my notes

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,8 +1,12 @@
-// ES6
+/* lecture 45 */
 
-class User {
-    constructor(username, email, password){
-        this.username = username;
+// ye saara js is after ES6 remember
+
+
+class User { /* basically though we know classes are just        syntatical sugar as mostly it is prototype dependent but still js now allows class just like c++ and java and all functionality will be same like them. */
+    constructor(username, email, password){ // constructor() will be called whenever you will create a new instance of class(or whenever you will use new keyword)
+
+        this.username = username;  // properties set 
         this.email = email;
         this.password = password
     }
@@ -18,10 +22,14 @@ class User {
 
 const chai = new User("chai", "chai@gmail.com", "123")
 
-console.log(chai.encryptPassword());
-console.log(chai.changeUsername());
+console.log(chai.encryptPassword()); // 123abc
+console.log(chai.changeUsername()); // CHAI
+
+
 
 // behind the scene
+
+/* here below we have assumed what we could do if we dont have class method  */
 
 function User(username, email, password){
     this.username = username;
@@ -41,3 +49,5 @@ const tea = new User("tea", "tea@gmail.com", "123")
 
 console.log(tea.encryptPassword());
 console.log(tea.changeUsername());
+
+/* lecture 45 continued aage  */
