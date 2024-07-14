7ab76933ebcb3c49da61872b1b5b573809558dee
GautamBajaj56 <gautam.bajaj.ug23@nsut.ac.in>
2024-07-14 14:18:50 +0530

    my notes

--- 10_classes_and_oop/inheritance.js
+++ 10_classes_and_oop/inheritance.js
@@ -1,30 +1,43 @@
+/* lecture 45 continued */
+
 class User {
-    constructor(username){
-        this.username = username
+    constructor(username) {
+        this.username = username; // Initializes the username property with the provided value
     }
 
-    logMe(){
-        console.log(`USERNAME is ${this.username}`);
-    }
-}
-
-class Teacher extends User{
-    constructor(username, email, password){
-        super(username)
-        this.email = email
-        this.password = password
-    }
-
-    addCourse(){
-        console.log(`A new course was added by ${this.username}`);
+    logMe() {
+        console.log(`USERNAME is ${this.username}`); // Logs the username to the console
     }
 }
 
-const chai = new Teacher("chai", "chai@teacher.com", "123")
+class Teacher extends User {  // extends implies that teacher class has now properties and methods of User class as well along with its own methods and properties
+    // inheritance shown thus.
 
-chai.logMe()
-const masalaChai = new User("masalaChai")
+    /* The extends keyword in JavaScript is used in class declarations or class expressions to create a class that is a child of another class. This allows the child class to inherit properties and methods from the parent class, enabling code reuse and the creation of hierarchical class structures. */
 
-masalaChai.logMe()
+    /* The super keyword in JavaScript is used in classes that extend other classes. It allows you to call functions on an object's parent class. */
 
-console.log(chai instanceof User);
+    constructor(username, email, password) { // here written a different constructor for Teacher class else User class ka hi inherit kar leta inherit 
+        super(username); // Calls the constructor of the User class to initialize the username
+        this.email = email; // Initializes the email property with the provided value
+        this.password = password; // Initializes the password property with the provided value
+    }
+
+    addCourse() {
+        console.log(`A new course was added by ${this.username}`); // Logs a message indicating a new course was added by the teacher
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123"); // Creates a new Teacher object with username, email, and password
+
+chai.logMe(); // USERNAME is chai
+
+const masalaChai = new User("masalaChai"); // Creates a new User object with the username "masalaChai"
+
+masalaChai.logMe(); // USERNAME is masalaChai
+
+console.log(chai instanceof User); // Checks if chai is an instance of User, returns true as though chai is an instance of Teacher but Teacher is child class of User.
+
+/* video 45 continued aage  */
+
+
--- 10_classes_and_oop/staticprop.js
+++ 10_classes_and_oop/staticprop.js
@@ -1,3 +1,11 @@
+/* lecture 45 continued */
+
+/* will learn about static keyword and its usage */
+
+/* The static keyword defines a static method or field for a class, or a static initialization block (see the link for more information about this usage). Static properties cannot be directly accessed on instances of the class. Instead, they're accessed on the class itself.
+
+Static methods are often utility functions, such as functions to create or clone objects, whereas static properties are useful for caches, fixed-configuration, or any other data you don't need to be replicated across instances. */
+
 class User {
     constructor(username){
         this.username = username
@@ -13,7 +21,9 @@ class User {
 }
 
 const hitesh = new User("hitesh")
-// console.log(hitesh.createId())
+// console.log(hitesh.createId()) Output: error as createID cannot be accessed by instances of class
+
+console.log(User.createId()) // 123
 
 class Teacher extends User {
     constructor(username, email){
@@ -23,4 +33,8 @@ class Teacher extends User {
 }
 
 const iphone = new Teacher("iphone", "i@phone.com")
-console.log(iphone.createId());
+//console.log(iphone.createId()); output: error 
+// this implies even via inheritance static properties and methods cannot be accessed.
+
+/* ++++++++++++++++++++++++++++lecture 45 finished+++++++++++++++++++++++++++++++++ */
+
